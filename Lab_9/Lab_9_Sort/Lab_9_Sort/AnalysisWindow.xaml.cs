0ba49d3923aa1f12b5007c410d38c3cf324123cc
Kam1nd <shamseevk12@email.com>
2026-04-18 00:11:42 +0300

    Commit 2

--- Lab_9/Lab_9_Sort/Lab_9_Sort/AnalysisWindow.xaml.cs
+++ Lab_9/Lab_9_Sort/Lab_9_Sort/AnalysisWindow.xaml.cs
@@ -9,22 +9,21 @@ namespace Lab_9_Sort
 {
     public partial class AnalysisWindow : Window
     {
-        public ObservableCollection<AnalysisResult> Results { get; set; } = new();
+        public ObservableCollection<AnalysisResult> Items { get; set; } = new();
 
         public AnalysisWindow()
         {
             InitializeComponent();
-            DgResults.ItemsSource = Results;
+            DgResults.ItemsSource = Items;
         }
 
         private void BtnBack_Click(object sender, RoutedEventArgs e) => Close();
 
         private void BtnAnalyze_Click(object sender, RoutedEventArgs e)
         {
-            Results.Clear();
+            Items.Clear();
             int[] sizes = { 10, 100, 150 };
             var rnd = new Random();
-            var sb = new StringBuilder();
 
             foreach (int size in sizes)
             {
@@ -35,32 +34,105 @@ namespace Lab_9_Sort
                 var sw1 = Stopwatch.StartNew();
                 new IntroSort().Algorithm(copy1);
                 sw1.Stop();
-                string introResult = $"С: {SortMetrics.ComparisonCount} П: {SortMetrics.PermutationCount} " +
-                                    $"В: {sw1.ElapsedMilliseconds} мс";
+                var introData = new Metrics((int)sw1.ElapsedMilliseconds, SortMetrics.ComparisonCount, SortMetrics.PermutationCount);
 
                 int[] copy2 = (int[])baseArray.Clone();
                 SortMetrics.Reset();
                 var sw2 = Stopwatch.StartNew();
                 new PigeonholeSort().Algorithm(copy2);
                 sw2.Stop();
-                string pigeonResult = $"С: {SortMetrics.ComparisonCount} П: {SortMetrics.PermutationCount} " +
-                                     $"В: {sw2.ElapsedMilliseconds} мс";
+                var pigeonData = new Metrics((int)sw2.ElapsedMilliseconds, SortMetrics.ComparisonCount, SortMetrics.PermutationCount);
 
-                Results.Add(new AnalysisResult
+                Items.Add(new AnalysisResult
                 {
                     Size = size,
-                    IntroSort = introResult,
-                    Pigeonhole = pigeonResult
+                    IntroSort = $"С: {introData.Comparisons} | П: {introData.Permutations} | В: {introData.Time} мс",
+                    Pigeonhole = $"С: {pigeonData.Comparisons} | П: {pigeonData.Permutations} | В: {pigeonData.Time} мс",
+                    IntroData = introData,
+                    PigeonData = pigeonData
                 });
             }
 
-            sb.AppendLine("• IntroSort эффективен для больших массивов (O(n log n)).");
-            sb.AppendLine("• PigeonholeSort не использует сравнения, эффективен при малом диапазоне значений.");
-            sb.AppendLine("• IntroSort требует меньше памяти, чем PigeonholeSort.");
-            sb.AppendLine("• PigeonholeSort работает за O(n + range), где range - диапазон значений.");
+            var sb = new StringBuilder();
+            sb.AppendLine("ПОДРОБНЫЙ СРАВНИТЕЛЬНЫЙ АНАЛИЗ:");
+            sb.AppendLine("--------------------------------------------------");
+
+            int introTotalPoints = 0;
+            int pigeonTotalPoints = 0;
+
+            foreach (var item in Items)
+            {
+                sb.AppendLine($" Размер массива: {item.Size} элементов");
+                sb.AppendLine();
+
+                if (item.IntroData.Time < item.PigeonData.Time)
+                {
+                    sb.AppendLine($"   Время: IntroSort быстрее ({item.IntroData.Time} мс < {item.PigeonData.Time} мс).");
+                    introTotalPoints++;
+                }
+                else if (item.PigeonData.Time < item.IntroData.Time)
+                {
+                    sb.AppendLine($"   Время: PigeonholeSort быстрее ({item.PigeonData.Time} мс < {item.IntroData.Time} мс).");
+                    pigeonTotalPoints++;
+                }
+                else
+                {
+                    sb.AppendLine($"   Время: Одинаковое ({item.IntroData.Time} мс).");
+                    introTotalPoints++;
+                    pigeonTotalPoints++;
+                }
+
+                if (item.IntroData.Comparisons < item.PigeonData.Comparisons)
+                {
+                    sb.AppendLine($"   Сравнений: IntroSort эффективнее ({item.IntroData.Comparisons}).");
+                    introTotalPoints++;
+                }
+                else if (item.PigeonData.Comparisons < item.IntroData.Comparisons)
+                {
+                    sb.AppendLine($"   Сравнений: PigeonholeSort эффективнее ({item.PigeonData.Comparisons}).");
+                    pigeonTotalPoints++;
+                }
+                else
+                {
+                    sb.AppendLine($"   Сравнений: Одинаково ({item.IntroData.Comparisons}).");
+                }
+
+                if (item.IntroData.Permutations < item.PigeonData.Permutations)
+                {
+                    sb.AppendLine($"   Перестановок: IntroSort эффективнее ({item.IntroData.Permutations}).");
+                    introTotalPoints++;
+                }
+                else if (item.PigeonData.Permutations < item.IntroData.Permutations)
+                {
+                    sb.AppendLine($"   Перестановок: PigeonholeSort эффективнее ({item.PigeonData.Permutations}).");
+                    pigeonTotalPoints++;
+                }
+                else
+                {
+                    sb.AppendLine($"   Перестановок: Одинаково ({item.IntroData.Permutations}).");
+                }
+
+                sb.AppendLine("--------------------------------------------------");
+            }
+
+            sb.AppendLine("ОБЩИЙ ИТОГ:");
+            if (introTotalPoints > pigeonTotalPoints)
+                sb.AppendLine("Победитель: INTRO SORT");
+            else if (pigeonTotalPoints > introTotalPoints)
+                sb.AppendLine("Победитель: PIGEONHOLE SORT");
+            else
+                sb.AppendLine("Ничья! Методы показали равную эффективность.");
 
             TxtAnalysis.Text = sb.ToString();
         }
+    }
+
+    public class Metrics
+    {
+        public int Time { get; }
+        public int Comparisons { get; }
+        public int Permutations { get; }
+        public Metrics(int t, int c, int p) { Time = t; Comparisons = c; Permutations = p; }
     }
 
     public class AnalysisResult
@@ -68,5 +140,7 @@ namespace Lab_9_Sort
         public int Size { get; set; }
         public string IntroSort { get; set; }
         public string Pigeonhole { get; set; }
+        public Metrics IntroData { get; set; }
+        public Metrics PigeonData { get; set; }
     }
 }
